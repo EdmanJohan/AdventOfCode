b7ad45516fb8c7e9bedea8803925274229850fa2
Johan Edman <22528843+EdmanJohan@users.noreply.github.com>
2021-11-28 20:59:23 +0100

    Initial commit.

---
+++ 2020/Program.cs
@@ -0,0 +1,16 @@
+﻿using AoCHelper;
+
+if (args.Length == 0)
+{
+    Solver.SolveLast();
+}
+else if (args.Length == 1 && args[0].Contains("all", System.StringComparison.CurrentCultureIgnoreCase))
+{
+    Solver.SolveAll();
+}
+else
+{
+    var indexes = args.Select(arg => uint.TryParse(arg, out var index) ? index : uint.MaxValue);
+
+    Solver.Solve(indexes.Where(i => i < uint.MaxValue));
+}
