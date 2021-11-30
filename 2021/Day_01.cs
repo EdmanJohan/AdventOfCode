189f9779b6430085c6a79ea007937aad596fa2f0
Johan Edman <22528843+EdmanJohan@users.noreply.github.com>
2021-11-30 20:35:45 +0100

    Update of dependencies and deleted files.

--- 2021/Day_01.cs
+++ 2021/Day_01.cs
@@ -1,18 +1,23 @@
-﻿using AoCHelper;
+﻿using System;
+using AoCHelper;
 
-namespace AdventOfCode
-{
-    public class Day_01 : BaseDay
-    {
+namespace AdventOfCode {
+    public class Day_01 : BaseDay {
         private readonly string _input;
 
-        public Day_01()
-        {
+        public Day_01() {
             _input = File.ReadAllText(InputFilePath);
         }
 
         public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
 
-        public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        public override async ValueTask<string> Solve_2() => new();
+
+        private async Task<string> CalculateIndex() {
+            await Task.Delay(200);
+            return "Solution 2";
+        }
+
+
     }
 }
