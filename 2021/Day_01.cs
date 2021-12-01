d52226f42f877c166ccf0ee041928777c6099937
Johan Edman <22528843+EdmanJohan@users.noreply.github.com>
2021-12-01 06:42:32 +0100

    2021 Day 1 Solved

--- 2021/Day_01.cs
+++ 2021/Day_01.cs
@@ -1,23 +1,47 @@
-﻿using System;
-using AoCHelper;
+﻿using AoCHelper;
 
 namespace AdventOfCode {
     public class Day_01 : BaseDay {
-        private readonly string _input;
+        private readonly List<int> _inputNumbers;
 
         public Day_01() {
-            _input = File.ReadAllText(InputFilePath);
+            if (!File.Exists(InputFilePath)) {
+                throw new SolvingException($"Path {InputFilePath} not found for {GetType().Name}");
+            }
+
+            _inputNumbers = File.ReadAllLines(InputFilePath).Where(x => int.TryParse(x, out var tempInt)).Select(t => int.Parse(t.Trim())).ToList();
+
         }
+        public override ValueTask<string> Solve_1() {
+            int? solution = 0;
 
-        public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+            int previousMeasurement = _inputNumbers[0];
+            for (var i = 1; i < _inputNumbers.Count; i++) {
+                var currentMeasurement = _inputNumbers[i];
+                if (currentMeasurement > previousMeasurement) {
+                    solution++;
+                }
 
-        public override async ValueTask<string> Solve_2() => new();
+                previousMeasurement = currentMeasurement;
+            }
 
-        private async Task<string> CalculateIndex() {
-            await Task.Delay(200);
-            return "Solution 2";
+            return new($"{solution.GetValueOrDefault()}");
         }
+        public override ValueTask<string> Solve_2() {
+            int? solution = 0;
 
+            int previousWindow = _inputNumbers[0] + _inputNumbers[1] + _inputNumbers[2];
+            for (var i = 1; i <_inputNumbers.Count - 2; i++) {
+                int currentWindow = _inputNumbers[i] + _inputNumbers[i + 1] + _inputNumbers[i + 2];
 
+                if (currentWindow > previousWindow) {
+                    solution++;
+                }
+
+                previousWindow = currentWindow;
+            }
+            
+            return new($"{solution.GetValueOrDefault()}");
+        }
     }
 }
